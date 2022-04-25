ac8cc222422c093d48c8aa77c439f28906967b9c
Przemo Dev <przemo.flutter@gmai.com>
2022-04-25 18:33:39 +0200

    sumting sumting

--- lib/domain/repositories/articles_repository.dart
+++ lib/domain/repositories/articles_repository.dart
@@ -4,7 +4,7 @@ import 'package:user_articles/domain/models/article_model.dart';
 class ArticlesRepository {
   ArticlesRepository({required this.remoteDataSource});
 
-  final ArticlesMockedDataSource remoteDataSource;
+  final AuthorsRemoteDioDataSource remoteDataSource;
 
   Future<List<ArticleModel>> getArticlesForAuthorId(int authorId) async {
     final json = await remoteDataSource.getArticles();
